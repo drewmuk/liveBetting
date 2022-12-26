72ab1196e16eba2ac1819bd94215f061bf689376
drewmuk <102569479+drewmuk@users.noreply.github.com>
2022-12-26 10:13:36 -0800

    Adding 2022 season play-by-play data

--- nfl/nflscrape.R
+++ nfl/nflscrape.R
@@ -18,15 +18,19 @@ formatPBP <- function(year) {
   pbp$spreadfactor <- pbp$spread_line * (pbp$game_seconds_remaining / 3600)
   pbp$OUfactor <- pbp$total_line * (pbp$game_seconds_remaining / 3600)
   
+  # offensive team's score after this drive
   pbp$es <- if_else(pbp$posteam_type=='away',pbp$total_away_score+pbp$ep,
                     pbp$total_home_score+pbp$ep)
-  
+
+  # defensive team's current score
   pbp$os <- if_else(pbp$posteam_type=='away',pbp$total_home_score,
                     pbp$total_away_score)
   
+  # expected MOV after this drive, incorporating the spread
   pbp$eMOV <- if_else(pbp$posteam_type=='away',pbp$es - pbp$os, 
                       pbp$os - pbp$es) + pbp$spreadfactor
   
+  # expected total after this drive, incorporating the over/under
   pbp$currentOU <- pbp$es + pbp$os + pbp$OUfactor
   
   pbp$finalMOV <- pbp$away_score - pbp$home_score
@@ -35,17 +39,18 @@ formatPBP <- function(year) {
   pbp
 }
 
+pbp22 <- formatPBP(2022)
 pbp21 <- formatPBP(2021)
 pbp20 <- formatPBP(2020)
 pbp19 <- formatPBP(2019)
 
-masterfile <- rbind(pbp21, pbp20, pbp19)
+masterfile <- rbind(pbp22, pbp21, pbp20, pbp19)
 
 MOVfit <- lm(finalMOV ~ eMOV + game_seconds_remaining, data=masterfile)
 OUfit <- lm(finalOU ~ currentOU + game_seconds_remaining + total_line, data=masterfile)
 
-write.csv(summary(MOVfit)$coefficients[,1],'C:/Users/drewm/liveBetting/nfl/MOVreg.csv')
-write.csv(summary(OUfit)$coefficients[,1],'C:/Users/drewm/liveBetting/nfl/OUreg.csv')
+write.csv(summary(MOVfit)$coefficients[,1],'C:/Users/drewm/Desktop/liveBetting/nfl/MOVreg.csv')
+write.csv(summary(OUfit)$coefficients[,1],'C:/Users/drewm/Desktop/liveBetting/nfl/OUreg.csv')
 
 
 # roof types (type of roof for each home team's stadium)
@@ -59,32 +64,37 @@ roof_df[which(roof_df$a=="LV" | roof_df$a=="NO" |
                 roof_df$a=="LAC" | roof_df$a=="MIN"),2] <- "dome"
 
 write.csv(roof_df,'C:/Users/drewm/Desktop/liveBetting/nfl/roof_df.csv',row.names = FALSE)
+# 
+# # slate info (number of games per slate)
+# 
+# slate_df <- data.frame(a = c(rep(c(1:18),5), c(4,5,8,10)),
+#                        b = c(rep("T",18), rep("S",54), rep("M",18), rep("S", 4)), 
+#                        c = c(rep("N",18), rep("M",18), rep("A",18),rep("N",36), rep("E",4)), 
+#                        d = c(rep(1,18), rep(9,18), rep(4,18), rep(1,36), rep(1,4)))
+# colnames(slate_df) <- c("Week","Day","Time","Games")
+# 
+# slate_df[which((slate_df$Week==6 | slate_df$Week==8 |
+#                   slate_df$Week==9 | slate_df$Week==11 |
+#                   slate_df$Week==13) & slate_df$Time=="M"),4] <- 8
+# 
+# slate_df[which((slate_df$Week==2 | slate_df$Week==7 |
+#                   slate_df$Week==10 | slate_df$Week==12 |
+#                   slate_df$Week==14) & slate_df$Time=="M"),4] <- 7
+# 
+# slate_df[which((slate_df$Week==4 | slate_df$Week==5 |
+#                  slate_df$Week==6 | slate_df$Week==10 |
+#                   slate_df$Week==11 | slate_df$Week==14 |
+#                   slate_df$Week==15) & slate_df$Time=="A"),4] <- 3
+# 
+# slate_df[which(slate_df$Week==2 & slate_df$Time=="A"),4] <- 5
+# slate_df[which(slate_df$Week==2 & slate_df$Day=="M"),4] <- 2
+# slate_df[which(slate_df$Week==9 & slate_df$Time=="A"),4] <- 2
+# slate_df[which(slate_df$Week==12 & slate_df$Day=="T"),4] <- 3
+# slate_df[which(slate_df$Week==15 & slate_df$Time=="M"),4] <- 5
+# 
+# write.csv(slate_df,'C:/Users/drewm/Desktop/liveBetting/nfl/slate_df.csv',row.names = FALSE)
 
-# slate info (number of games per slate)
-
-slate_df <- data.frame(a = c(rep(c(1:18),5), c(4,5,8,10)),
-                       b = c(rep("T",18), rep("S",54), rep("M",18), rep("S", 4)), 
-                       c = c(rep("N",18), rep("M",18), rep("A",18),rep("N",36), rep("E",4)), 
-                       d = c(rep(1,18), rep(9,18), rep(4,18), rep(1,36), rep(1,4)))
-colnames(slate_df) <- c("Week","Day","Time","Games")
-
-slate_df[which((slate_df$Week==6 | slate_df$Week==8 |
-                  slate_df$Week==9 | slate_df$Week==11 |
-                  slate_df$Week==13) & slate_df$Time=="M"),4] <- 8
-
-slate_df[which((slate_df$Week==2 | slate_df$Week==7 |
-                  slate_df$Week==10 | slate_df$Week==12 |
-                  slate_df$Week==14) & slate_df$Time=="M"),4] <- 7
-
-slate_df[which((slate_df$Week==4 | slate_df$Week==5 |
-                 slate_df$Week==6 | slate_df$Week==10 |
-                  slate_df$Week==11 | slate_df$Week==14 |
-                  slate_df$Week==15) & slate_df$Time=="A"),4] <- 3
-
-slate_df[which(slate_df$Week==2 & slate_df$Time=="A"),4] <- 5
-slate_df[which(slate_df$Week==2 & slate_df$Day=="M"),4] <- 2
-slate_df[which(slate_df$Week==9 & slate_df$Time=="A"),4] <- 2
-slate_df[which(slate_df$Week==12 & slate_df$Day=="T"),4] <- 3
-slate_df[which(slate_df$Week==15 & slate_df$Time=="M"),4] <- 5
-
-write.csv(slate_df,'C:/Users/drewm/Desktop/liveBetting/nfl/slate_df.csv',row.names = FALSE)
+allgames21 <- pbp21[!duplicated(pbp21$game_id),]
+allgames21$home_win <- ifelse(allgames21$home_score>allgames21$away_score,1,0)
+allgames21 <- allgames21[,c('away_team','home_team','spread_line','home_win')]
+write.csv(allgames21,'C:/Users/drewm/Desktop/liveBetting/nfl/allgames21.csv',row.names = FALSE)
